c4ab35c1e121fb34b0955a8594a095ba00fbd54c
mdign002 <78434360+mdign002@users.noreply.github.com>
2021-07-14 10:35:10 -0400

    Update htmlsave.py
    Revised the createfilename() function to include a variable for defining the separator between author ID and date.
    Placed separator before author ID to compensate for IDs that begin with hyphens.
    Replaced double quotation marks " with single quotation marks ' throughout to be consistent with html2ors.py program.
    Defined id_separator = 'XXXXXXX'

--- htmlsave.py
+++ htmlsave.py
@@ -7,14 +7,15 @@ import requests
 from datetime import date 
 
 def createfilename():
-    filename = (authorID + "-" + str(today) + "-" + str("%04d" % begin_value) + \
-                         "-" + str("%04d" % end_value) + ".html")
+    id_separator = 'XXXXXXX'
+    filename = (id_separator + authorID + id_separator + str(today) + '-' + str('%04d' % begin_value) + \
+                         '-' + str('%04d' % end_value) + '.html')
     return filename
 
 def createURL():
-    captureURL = ("https://scholar.google.com/citations?hl=en&user=" + authorID + \
-                 "&view_op=list_works&sortby=pubdate&cstart=" + str(begin_value) + \
-                 "&pagesize=" + str(end_value))
+    captureURL = ('https://scholar.google.com/citations?hl=en&user=' + authorID + \
+                 '&view_op=list_works&sortby=pubdate&cstart=' + str(begin_value) + \
+                 '&pagesize=' + str(end_value))
     return captureURL
 
 #Import author IDs from command line and download Google Scholar webpages
@@ -22,7 +23,7 @@ arguments = len(sys.argv)
 today = date.today()
 for a in range(1,arguments):
     authorID = sys.argv[a]
-    sys.stdout.write("Processing Author ID " + authorID + " . . .\n")
+    sys.stdout.write('Processing Author ID ' + authorID + ' . . .\n')
 
     #Loop through the program to download author ID webpages
     begin_value = 0
@@ -35,14 +36,14 @@ for a in range(1,arguments):
         begin_value = begin_value + 1
         new_filename = createfilename()
         if os.path.exists(new_filename):
-            sys.stdout.write("Overwriting existing file with same name ...\n")
+            sys.stdout.write('Overwriting existing file with same name ...\n')
         else:
-            sys.stdout.write("Creating new file ...\n")
-        html_file = open(new_filename, "wb")
+            sys.stdout.write('Creating new file ...\n')
+        html_file = open(new_filename, 'wb')
         html_file.write(page.content)
         html_file.close()
-        sys.stdout.write("File saved as \"" + html_file.name + "\"\n")
+        sys.stdout.write('File saved as '" + html_file.name + '"\n')
     if statuscode != 200:
-        sys.stderr.write("Incorrect author ID or inaccessible webpage.\n")
+        sys.stderr.write('Incorrect author ID or inaccessible webpage.\n')
         continue
     
