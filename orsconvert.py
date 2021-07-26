724b30f14f617d2a0d1a34c8310b5696ba72d552
mdign002 <78434360+mdign002@users.noreply.github.com>
2021-07-26 14:04:13 -0400

    Update orsconvert.py
    Program revised to process all types: json, bibtex, or html. 
    Program revised to have each conversion a called function.

--- orsconvert.py
+++ orsconvert.py
@@ -3,15 +3,7 @@ import sys
 import argparse
 import fileinput
 
-parser = argparse.ArgumentParser(description='Converts ORS file to selected filetype')
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('--json', action='store_true', help='Converts to JSON format')
-group.add_argument('--bibtex', action='store_true', help='Converts to BIBTEX format')
-group.add_argument('--html', action='store_true', help='Converts to HTML format')
-parser.add_argument('inputfile', type=str, nargs='?', help='enter the ORS file name')
-args = parser.parse_args()
-
-if args.json:
+def createjson():
     for line in fileinput.input(args.inputfile):
         item_hash,item_year,item_list = line.split(' ', 2)
         item_list = item_list.replace('":"', '": "')
@@ -31,14 +23,13 @@ if args.json:
         pageyear = pageyear.replace('": "', '": ')
         pageyear = pageyear.replace('"}\n', '')
         pageyear = ('"' + pageyear)
-
         json_entry = (item_hash + ' ' + item_year + ' \n{ \n    ' + directURL + ',\n    ' + \
                       title + ',\n    ' + authors + ',\n    ' + source + ',\n    ' + \
                       citedby + ',\n    ' + citations + ',\n    ' + pageyear + \
                       '\n}\n')
         sys.stdout.write(json_entry +"\n")
-            
-elif args.bibtex:
+
+def createbibtex():
     for line in fileinput.input(args.inputfile):
         item_hash,item_year,item_list = line.split(' ', 2)
         directURL, title, authors, source, citedby, citations, pageyear = item_list.split('", "')
@@ -55,21 +46,21 @@ elif args.bibtex:
                         directURL + '},\n},\n')
         sys.stdout.write(bibtex_entry +"\n")
 
-elif args.html:
+def createhtml():
     sys.stdout.write('<html>\n')
-    sys.stdout.write('  <head>\n')
-    sys.stdout.write('    <title>Web Sciences and Digital Libraries Research Group</title>\n')
-    sys.stdout.write('    <link rel="stylesheet" type="text/css" href="https://www.odu.edu:>\n')
-    sys.stdout.write('  </head>\n')
-    sys.stdout.write('  <body bgcolor="white">\n')
-    sys.stdout.write('    <script type="text/javascript">...</script>\n')
-    sys.stdout.write('    <script src="https://ssl.google-analytics.com/ga.js" \
-                                            type="text/javascript">...</script>\n')
-    sys.stdout.write('<script type=\n  ')
-    sys.stdout.write('</head>\n  ')
-    sys.stdout.write('</head>\n  ')
-    sys.stdout.write('</head>\n  ')
-
+    sys.stdout.write('<head>\n')
+    sys.stdout.write('<title>Web Sciences and Digital Libraries Research Group</title>\n')
+    sys.stdout.write('<link rel="stylesheet" type="text/css" href="https://www.odu.edu:"></head>\n')
+    sys.stdout.write('<body bgcolor="white">\n')
+    sys.stdout.write('<script type="text/javascript"> var gaJsHost = (("https:" == document.location.protocol) ? \
+                     "https://ssl." : "http://www.");document.write(unescape("%3Cscript src=\'" + gaJsHost + \
+                     "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));</script>\n')
+    sys.stdout.write('<script src="https://ssl.google-analytics.com/ga.js" type="text/javascript"></script>\n')
+    sys.stdout.write('<script type="text/javascript"> var pageTracker = _get._getTracker("UA-5691952-1"); \
+                     pageTracker._trackPageview();</script>\n')
+    sys.stdout.write('<h2>Web Sciences and Digital Libraries Research Group</h2>\n')
+    sys.stdout.write('<p> </p>\n')
+    sys.stdout.write('<ol>\n')
     for line in fileinput.input(args.inputfile):
         item_hash,item_year,item_list = line.split(' ', 2)
         directURL, title, authors, source, citedby, citations, pageyear = item_list.split('", "')
@@ -79,8 +70,29 @@ elif args.html:
         source = source.replace('Source":"', '')
         citedby = citedby.replace('CitedBy":"', '')
         pageyear = pageyear.replace('PageYear":"', '') 
-        pageyear = pageyear.replace('"}\n', '') 
-        print('Saving in HTML format . . .')
+        pageyear = pageyear.replace('"}\n', '')
+        sys.stdout.write('<li>' + authors + ', <b><a href="' + directURL + '">' + title + '</a></b>, ' + \
+                         source + ', ' + pageyear + '.<p> </p></li>\n')
+    sys.stdout.write('</ol>\n')
+    sys.stdout.write('</body>\n')
+    sys.stdout.write('</html>\n') 
 
+parser = argparse.ArgumentParser(description='Converts ORS file to selected filetype')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--json', action='store_true', help='Converts to JSON format')
+group.add_argument('--bibtex', action='store_true', help='Converts to BIBTEX format')
+group.add_argument('--html', action='store_true', help='Converts to HTML format')
+parser.add_argument('inputfile', type=str, nargs='?', help='enter the ORS file name')
+args = parser.parse_args()
+
+if args.json:
+    createjson()
+           
+elif args.bibtex:
+    createbibtex()
+
+elif args.html:
+    createhtml()
+ 
 
     
