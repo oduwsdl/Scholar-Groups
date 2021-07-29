503642f26953ab8d2c0bc659a187038d5d3e773d
mdign002 <78434360+mdign002@users.noreply.github.com>
2021-07-29 14:27:57 -0400

    Update dedup.py
    Comments have been added to the code to describe the construction and operation. Additionally, the code can easily be revised to display duplicates instead of unique entries if desired.

--- dedup.py
+++ dedup.py
@@ -4,15 +4,30 @@
 import sys
 import fileinput
 
-#Import the ORS file contents and compare hash key values
+"""
+This program examines entries in an ORS file to remove duplicates. The 
+program is designed to take a filename as input or to read lines from 
+the Command Line Interface. Currently, it displays unique entries using 
+the STDOUT function. However, it can easily be revised to display those 
+entries that are duplicates. The program functions by examining the 
+first key in the ORS file and storing it in an array. After that first 
+entry is stored, the hash key value is extracted from each line and 
+compared with the array. If no duplicate entry is found, the line is 
+sent to the STDOUT function, and the hash key is added to the array. If 
+the hash is already found in the array, the link is skipped.  
+"""
+
+# Import the ORS file contents and compare hash key values
 ors_comp = []
 first_word = []
 duplicates = []
-    for line in fileinput.input():
-        firstword = line.split(' ', 1)[0]
-        if firstword in first_word:
-            duplicates.append(firstword)
-        if firstword not in first_word:
-            first_word.append(firstword)
-            ors_comp.append(line)
-            sys.stdout.write(line)
+for line in fileinput.input():
+    firstword = line.split(' ', 1)[0]
+    if firstword in first_word:
+        duplicates.append(firstword)
+        # sys.stdout.write(line)  # Used as needed to list duplicates
+
+    if firstword not in first_word:
+        first_word.append(firstword)
+        ors_comp.append(line)
+        sys.stdout.write(line)
