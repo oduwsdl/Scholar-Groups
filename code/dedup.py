919ec9966a7ab2de8cb5c26d19366759bd5d794a
Evan Savaria <esava005@odu.edu>
2021-10-27 12:12:07 -0400

    UKVS Update and fixed bugs
    Replaced ORS with UKVS and fixed README.md issues.

---
+++ code/dedup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#dedup.py
+
+import sys
+import fileinput
+
+"""
+This program examines entries in an UKVS file to remove duplicates. The 
+program is designed to take a filename as input or to read lines from 
+the Command Line Interface. Currently, it displays unique entries using 
+the STDOUT function. However, it can easily be revised to display those 
+entries that are duplicates. The program functions by examining the 
+first key in the UKVS file and storing it in an array. After that first 
+entry is stored, the hash key value is extracted from each line and 
+compared with the array. If no duplicate entry is found, the line is 
+sent to the STDOUT function, and the hash key is added to the array. If 
+the hash is already found in the array, the link is skipped.  
+"""
+
+# Import the UKVS file contents and compare hash key values
+ukvs_comp = []
+first_word = []
+duplicates = []
+for line in fileinput.input():
+    firstword = line.split(' ', 1)[0]
+    if firstword in first_word:
+        duplicates.append(firstword)
+        # sys.stdout.write(line)  # Used as needed to list duplicates
+
+    if firstword not in first_word:
+        first_word.append(firstword)
+        ukvs_comp.append(line)
+        sys.stdout.write(line)
