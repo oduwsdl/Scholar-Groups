4c28e532c4c06bbfc629ae7ff9a50a0de2fac2f5
Michael L. Nelson <rhodewarriors@gmail.com>
2024-03-03 19:02:16 -0500

    Update htmlsave.py
    testing to see if this still runs.

--- code/htmlsave.py
+++ code/htmlsave.py
@@ -69,7 +69,7 @@ if arguments == 3:
     sys.stdout.write('No author IDs provided to process ...\n')
 for a in range(3,arguments):
     authorID = sys.argv[a]
-    sys.stdout.write('Processing Author ID ' + authorID + ' ...\n')
+    sys.stdout.write('Processing Author ID ' + authorID + ' ...\n') 
 
     # Loop through the program to download author ID webpages. Currently, the
     # Google Scholar website allows capturing of up to 100 articles at a time.
